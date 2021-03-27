9566be03e25a1af8d64fb3664ababaeeca395bd4
Bill Nixon <bnixon67@gmail.com>
2021-03-27 10:48:52 -0500

    updated build process

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 for SRC in *.go
 do
 	echo ${SRC}
-	GOOS=linux GOARCH=amd64 go build ${SRC}
-	GOOS=windows GOARCH=amd64 go build ${SRC}
+	BASE=${SRC%.*}
+	GOOS=linux GOARCH=amd64 go build -o bin/${BASE}.linux.amd64 ${SRC}
+	GOOS=freebsd GOARCH=amd64 go build -o bin/${BASE}.freebsd.amd64 ${SRC}
+	GOOS=windows GOARCH=amd64 go build -o bin/${BASE}.exe ${SRC}
 done
